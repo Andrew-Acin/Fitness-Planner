9297027e914d40b421d7c5af822ed507db00b187
astonelv <158239756+astonelv@users.noreply.github.com>
2024-09-30 20:34:16 -0700

    update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 
